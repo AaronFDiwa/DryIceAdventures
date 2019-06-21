a076feb748e3330aa0d1eaa32cc0976d519d0a79
AaronFDiwa <aaron4diwa@gmail.com>
2019-06-21 14:32:48 -0600

    add player movement

---
+++ Dry Ice Adventures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Config Parameters
+    int movementSpeed = 10;
+    private float moveX;
+    private float moveY;
+
+    // Cached References
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        myRigidbody.velocity = new Vector2(moveX * movementSpeed, moveY * movementSpeed);
+    }
+}
